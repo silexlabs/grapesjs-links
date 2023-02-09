ae6ce541c859d2d489ac0a813957a9ff8ca4925b
Alex Hoyau <alex@lexoyo.me>
2023-02-09 13:59:23 +0100

    start split from silex

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import en from './locale/en';
+
+export default (editor, opts = {}) => {
+  const options = { ...{
+    i18n: {},
+    // default options
+  },  ...opts };
+
+  
+  
+  // Load i18n files
+  editor.I18n && editor.I18n.addMessages({
+      en,
+      ...options.i18n,
+  });
+
+  // TODO Remove
+  editor.on('load', () =>
+    editor.addComponents(
+        `<div style="margin:100px; padding:25px;">
+            Content loaded from the plugin
+        </div>`,
+        { at: 0 }
+    ))
+};
---
+++ src/locale/en.js
@@ -0,0 +1,5 @@
+export default {
+    '@silexlabs/grapesjs-links': {
+        // 'key': 'value',
+    },
+};
